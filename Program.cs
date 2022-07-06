ed5015467e276de94a0f5fb4e4963219b912276d
Shtirlits1972 <shtirlits1972@gmail.com>
2022-07-07 01:04:36 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+//  using Microsoft.AspNetCore.WebSockets.Client;
+
+namespace SocketTest1
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string[] strbtc = new string[1];
+            strbtc[0] = "BTC-USD";
+            //  strbtc[1] = "BTC-EUR";
+
+            channel ch1 = new channel { name = "ticker", product_ids = strbtc };
+            channel[] chArray = new channel[1];
+            chArray[0] = ch1;
+            Wshello wshello = new Wshello { type = "subscribe", channels = chArray };
+
+            string message = JsonConvert.SerializeObject(wshello);
+
+
+            ClientWebSocket websocket = new ClientWebSocket();
+            string url = "wss://ws-feed.pro.coinbase.com";
+            Console.WriteLine("Connecting to: " + url);
+            await websocket.ConnectAsync(new Uri(url), CancellationToken.None);
+
+            Console.WriteLine("Sending message: " + message);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            await websocket.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            Console.WriteLine();
+            Console.WriteLine("wait for a response");
+            Console.WriteLine();
+
+
+            while (true)
+            {
+                byte[] incomingData = new byte[1024];
+                WebSocketReceiveResult result = await websocket.ReceiveAsync(new ArraySegment<byte>(incomingData), CancellationToken.None);
+
+                if (result.CloseStatus.HasValue)
+                {
+                    Console.WriteLine("Closed; Status: " + result.CloseStatus + ", " + result.CloseStatusDescription);
+                }
+                else
+                {
+                    Console.WriteLine("Received message: " + Encoding.UTF8.GetString(incomingData, 0, result.Count));
+                }
+
+                Console.WriteLine("==========================================================");
+                Thread.Sleep(1000);
+            }
+
+
+        }
+
+    }
+}
---
+++ Wshello.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketTest1
+{
+    public class Wshello
+    {
+        public  string type { get; set; }
+        public  channel [] channels { get; set; }
+    }
+
+
+    public class channel
+    {
+        public  string name { get; set; }
+        public  string[] product_ids { get; set; }
+    }
+}
